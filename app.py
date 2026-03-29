e43efbb3582b6965175fac0fb6d89f73f5e8a667
mahesh <maheshbabuvishnumolakala@gmail.com>
2026-03-29 20:10:35 +0530

    learning how to deploy in HuggingFace

---
+++ app.py
@@ -0,0 +1,24 @@
+import gradio as gr
+from transformers import pipeline
+
+bert_model = pipeline("sentiment-analysis")
+distilbert_model = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
+
+def compare_models(text):
+    result1 = bert_model(text)[0]
+    result2 = distilbert_model(text)[0]
+
+    return(
+        f"BERT -> {result1['Label']} {round(result1['Score'],2)}\n"
+        f"DistilBERT -> {result2['Label']} {round(result2['Score'],2)}"
+    )
+
+iface = gr.Interface(
+    fn=compare_models,
+    inputs=gr.Textbox(label="Enter Text"),
+    outputs="text",
+    title="NLP Model Comparision",
+    description="Compare predictions of multiple NLP models"
+)
+
+iface.launch()
